082a0f5aae0d64edd8644ff690729a15c53cc480
zhangjunli <1217105106@qq.com>
2021-09-14 18:31:16 +0800

    [update]change session to conn
    Signed-off-by: zhangjunli <1217105106@qq.com>

--- conn.go
+++ conn.go
@@ -1,1 +1,18 @@
 package goNet
+
+type Conn interface {
+	//原始套接字
+	Socket() interface{}
+	// 发送消息，消息需要以指针格式传入
+	Send(msg interface{})
+	// 断开
+	Close()
+	// ID
+	ID() uint64
+	//数据存储
+	Value(obj ...interface{}) interface{}
+	//添加场景,如果场景相同会进行覆盖
+	JoinScene(sceneID uint8, scene Scene)
+	//获取场景
+	GetScene(sceneID uint8) Scene
+}
--- session.go
+++ session.go
@@ -11,24 +11,25 @@ var (
 	sessionType reflect.Type
 )
 
+//单次会话
+type Session interface {
+	//原始套接字
+	Socket() interface{}
+	// 发送消息，消息需要以指针格式传入
+	Send(msg interface{})
+	// 断开
+	Close()
+	// ID
+	ID() uint64
+	//数据存储
+	Value(obj ...interface{}) interface{}
+	//添加场景,如果场景相同会进行覆盖
+	JoinScene(sceneID uint8, scene Scene)
+	//获取场景
+	GetScene(sceneID uint8) Scene
+}
+
 type (
-	//会话
-	Session interface {
-		//原始套接字
-		Socket() interface{}
-		// 发送消息，消息需要以指针格式传入
-		Send(msg interface{})
-		// 断开
-		Close()
-		// ID
-		ID() uint64
-		//数据存储
-		Value(obj ...interface{}) interface{}
-		//添加场景,如果场景相同会进行覆盖
-		JoinScene(sceneID uint8, scene Scene)
-		//获取场景
-		GetScene(sceneID uint8) Scene
-	}
 
 	//核心会话标志
 	SessionIdentify struct {
